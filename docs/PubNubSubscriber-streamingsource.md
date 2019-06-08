23accc09fd94fba5f6381588800d764d8bf1d587
Nitin Motgi <nmotgi@nmotgi-macbookpro2.roam.corp.google.com>
2019-06-08 00:46:45 -0700

    Added documentation and Java docs

--- docs/PubNubSubscriber-streamingsource.md
+++ docs/PubNubSubscriber-streamingsource.md
@@ -1,1 +1,84 @@
-# PubNub Subscriber
+#### **Description**
+
+Reads realtime events from PubNub cloud.
+
+#### **Usage**
+
+PubNub is a programmable network for developing realtime applications;
+an evolution from three-tier architecture, purpose-built to handle all
+the complexities of data streams. PubNub operates at the edge of the
+network to handle and apply logic to real-time data, thereby minimizing
+latency to 250 milliseconds or less worldwide, and guaranteeing reliability
+and scalability.
+
+PubNub source can be used for retrieving events from the channels in realtime.
+
+### Basic
+
+#### **Properties**
+
+* **Reference Name**
+
+  Name used to uniquely identify this sink for lineage, annotating metadata, etc.
+
+* **Channels**
+
+  Dataset the tables belongs to. A dataset is contained within a specific project.
+Datasets are top-level containers that are used to organize and control access to tables and views.
+If dataset does not exist, it will be created.
+
+* **Subscriber Key**
+
+  Google Cloud Storage bucket to store temporary data in.
+It will be automatically created if it does not exist, but will not be automatically deleted.
+Temporary data will be deleted after it is loaded into BigQuery. If it is not provided, a unique
+bucket will be created and then deleted after the run finishes.
+
+### Advanced
+
+#### **Properties**
+
+* **Secure**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Reconnection Policy**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Connection Timeout**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Max Reconnect Attempt**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Use Proxy**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Type of Proxy**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Proxy Hostname**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Proxy Port**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
+
+* **Authorization Key**
+
+  The name of the field that will be used to determine which table to write to.
+Defaults to `tablename`.
--- src/main/java/io/cdap/plugin/pubnub/PubNubSubscriber.java
+++ src/main/java/io/cdap/plugin/pubnub/PubNubSubscriber.java
@@ -18,16 +18,18 @@ package io.cdap.plugin.pubnub;
 
 import com.google.common.collect.Sets;
 import com.pubnub.api.PNConfiguration;
-import com.pubnub.api.enums.PNLogVerbosity;
 import com.pubnub.api.enums.PNReconnectionPolicy;
 import io.cdap.cdap.api.annotation.Description;
 import io.cdap.cdap.api.annotation.Name;
 import io.cdap.cdap.api.annotation.Plugin;
 import io.cdap.cdap.api.data.format.StructuredRecord;
 import io.cdap.cdap.api.data.schema.Schema;
+import io.cdap.cdap.api.dataset.DatasetProperties;
 import io.cdap.cdap.etl.api.PipelineConfigurer;
 import io.cdap.cdap.etl.api.streaming.StreamingContext;
 import io.cdap.cdap.etl.api.streaming.StreamingSource;
+import io.cdap.plugin.common.Constants;
+import io.cdap.plugin.common.IdUtils;
 import io.cdap.plugin.common.ReferencePluginConfig;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.storage.StorageLevel;
@@ -48,7 +50,17 @@ import java.util.Set;
 import javax.annotation.Nullable;
 
 /**
- * PubNub subscriber.
+ * A PubNub subscriber for reading real-time messages.
+ *
+ * <p>This class <code>PubNubSubscriber</code> leverages cloud infrastructure of PubNub. The class
+ * defines a pre-defined schema containing channel, publisher, payload, subscription and timetoken.</p>
+ *
+ * <p>Uses the <code>PNConfiguration</code> to configure the PubNub using the Java client. It sets
+ * important configuration like connection timeout, reconnect attempts, proxy setting, and also authorization
+ * key.</p>
+ *
+ * @see PNConfiguration
+ * @see PubNubUtils
  */
 @Plugin(type = StreamingSource.PLUGIN_TYPE)
 @Name("PubNubSubscriber")
@@ -60,44 +72,72 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
   private static final String TIMETOKEN = "timetoken";
   private static final String PUBLISHER = "publisher";
   private static final String PAYLOAD = "payload";
+  private static final String SUBSCRIPTION = "subscription";
+  private static final int MAX_RECONNECT_ATTEMPTS = 5;
+  private static final int CONNECT_TIMEOUT_IN_SECONDS = 5;
 
+  /**
+   * Defines the pre-defined output schema for the PubNub subscriber.
+   */
   public static final Schema SUBSCRIBER_SCHEMA = Schema.recordOf("subscriber",
     Schema.Field.of(CHANNEL, Schema.nullableOf(Schema.of(Schema.Type.STRING))),
     Schema.Field.of(TIMETOKEN, Schema.of(Schema.Type.LONG)),
     Schema.Field.of(PUBLISHER, Schema.nullableOf(Schema.of(Schema.Type.STRING))),
-    Schema.Field.of(PAYLOAD, Schema.nullableOf(Schema.of(Schema.Type.STRING)))
+    Schema.Field.of(PAYLOAD, Schema.nullableOf(Schema.of(Schema.Type.STRING))),
+    Schema.Field.of(SUBSCRIPTION, Schema.nullableOf(Schema.of(Schema.Type.STRING)))
   );
 
+  /**
+   * Constructor to initialize <code>PubNubSubscriber</code>.
+   *
+   * @param conf a <code>PubNubConfig</code> specifying the configuration of plugin.
+   */
   public PubNubSubscriber(PubNubConfig conf) {
     this.config = conf;
   }
 
+  /**
+   * Configures the <code>PubNubSubscriber</code>.
+   * This methods registers a external PubNub Dataset as well as sets the output schema of the plugin.
+   *
+   * @param configurer a <code>PipelineConfigurer</code> for configuring the plugin during deployment phase.
+   * @throws IllegalArgumentException thrown when there are issues with the configurations.
+   */
   @Override
   public void configurePipeline(PipelineConfigurer configurer) throws IllegalArgumentException {
     super.configurePipeline(configurer);
+    IdUtils.validateId(config.referenceName);
+    configurer.createDataset(config.referenceName, Constants.EXTERNAL_DATASET_TYPE, DatasetProperties.EMPTY);
     configurer.getStageConfigurer().setOutputSchema(SUBSCRIBER_SCHEMA);
   }
 
-
+  /**
+   * Reads the events from PubNub using the Java receiver.
+   *
+   * @param context a <code>StreamingContext</code> for accessing Spark streaming context.
+   * @return a <code>JavaDStream</code> instance.
+   * @throws Exception throw when there is issue configuring or retriveing events from PubNub.
+   */
   @Override
   public JavaDStream<StructuredRecord> getStream(StreamingContext context) throws Exception {
     context.registerLineage(config.referenceName);
 
-
     PNConfiguration pnConfig = new PNConfiguration();
     pnConfig.setSubscribeKey(config.getSubscriberKey());
     pnConfig.setSecure(config.isSecure());
-    if (config.getConnectionTimeout() != -1) {
-      pnConfig.setConnectTimeout(config.getConnectionTimeout());
-    }
+    pnConfig.setConnectTimeout(config.getConnectionTimeout());
     pnConfig.setReconnectionPolicy(config.getReconnectionPolicy());
+    pnConfig.setMaximumReconnectionRetries(config.getMaxReconnectAttempts());
 
     if (config.hasProxy()) {
-      SocketAddress proxyAddress = new InetSocketAddress(config.getProxyHostname(), config.getProxyPort());
-      pnConfig.setProxy(new Proxy(Proxy.Type.HTTP, proxyAddress));
+      if (config.getProxyHostname() != null && config.getProxyPort() != null) {
+        SocketAddress proxyAddress = new InetSocketAddress(config.getProxyHostname(), config.getProxyPort());
+        pnConfig.setProxy(new Proxy(config.getProxyType(), proxyAddress));
+      } else {
+        throw new IllegalArgumentException("Proxy enabled, but hostname or port not specified.");
+      }
     }
 
-    pnConfig.setLogVerbosity(PNLogVerbosity.NONE);
     if (config.getAuthKey() != null) {
       pnConfig.setAuthKey(config.getAuthKey());
     }
@@ -115,6 +155,7 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
           if (channel != null) {
             builder.set(CHANNEL, channel);
           }
+          builder.set(SUBSCRIPTION, message.getSubscription());
           builder.set(TIMETOKEN, message.getTimestamp());
           builder.set(PUBLISHER, message.getPublisher());
           builder.set(PAYLOAD, message.getPayload());
@@ -143,9 +184,14 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
     private String subscriberKey;
 
     @Name("connection-timeout")
-    @Description("Connection timeout")
+    @Description("Maximum number of seconds which the client should wait for connection before timing out")
     @Nullable
     private Integer connectionTimeout;
+
+    @Name("max-reconnect-attempts")
+    @Description("Set how many times the reconneciton manager will try to connect before giving up")
+    @Nullable
+    private Integer maxReconnectAttempts;
 
     @Name("proxy-hostname")
     @Description("Proxy hostname")
@@ -158,12 +204,12 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
     private Integer proxyPort;
 
     @Name("secure")
-    @Description("Connect using HTTPS")
+    @Description("Switch the client to HTTPS based communications")
     @Nullable
     private Boolean secure;
 
     @Name("reconnection-policy")
-    @Description("Connect using HTTPS")
+    @Description("Reconnection policy which will be used if/when networking goes down")
     @Nullable
     private String reconnectionPolicy;
 
@@ -171,6 +217,11 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
     @Description("Authorizaton key")
     @Nullable
     private String authKey;
+
+    @Name("proxy-type")
+    @Description("Specify the type of proxy")
+    @Nullable
+    private String proxyType;
 
     @Name("use-proxy")
     @Description("Specify a proxy to be used to connect to PubNub")
@@ -188,28 +239,84 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
       this.reconnectionPolicy = "linear";
       this.connectionTimeout = -1;
       this.useProxy = false;
+      this.maxReconnectAttempts = 5;
     }
 
+    /**
+     * @return a <code>Set</code> of channels to be connected using the subscription key.
+     */
     public Set<String> getChannels() {
       return Sets.newHashSet(Arrays.asList(channels.split(",")));
     }
 
+    /**
+     * @return a <code>String</code> specifying the subscriber key used to connect to PubNub.
+     */
     public String getSubscriberKey() {
       return subscriberKey;
     }
 
+    /**
+     * @return a <code>Integer</code> specifying the connection timeout in seconds.
+     */
     public int getConnectionTimeout() {
-      return connectionTimeout;
+      if (connectionTimeout != null) {
+        return connectionTimeout;
+      }
+      return CONNECT_TIMEOUT_IN_SECONDS;
     }
 
+    /**
+     * @return a <code>int</code> specifying the max reconnect attempts before giving up.
+     */
+    public int getMaxReconnectAttempts() {
+      if (maxReconnectAttempts != null) {
+        return maxReconnectAttempts;
+      }
+      return MAX_RECONNECT_ATTEMPTS;
+    }
+
+    public Proxy.Type getProxyType() {
+      if (proxyType.equalsIgnoreCase("http")) {
+        return Proxy.Type.HTTP;
+      }
+
+      if (proxyType.equalsIgnoreCase("direct")) {
+        return Proxy.Type.DIRECT;
+      }
+
+      if (proxyType.equalsIgnoreCase("socks")) {
+        return Proxy.Type.SOCKS;
+      }
+
+      return Proxy.Type.HTTP;
+    }
+
+    /**
+     * @return a <code>Boolean</code> type specifying whether proxy is required.
+     */
+    @Nullable
+    public boolean hasProxy() {
+      return useProxy;
+    }
+
+    /**
+     * @return a <code>String</code> specifying the hostname of the proxy to be used to connect.
+     */
     public String getProxyHostname() {
       return proxyHostname;
     }
 
-    public int getProxyPort() {
+    /**
+     * @return a <code>Integer</code> specifying the proxy port.
+     */
+    public Integer getProxyPort() {
       return proxyPort;
     }
 
+    /**
+     * @return a instance of <code>PNReconnectPolicy</code> specifying how the client should backoff before reconnect.
+     */
     public PNReconnectionPolicy getReconnectionPolicy() {
       if (reconnectionPolicy.equalsIgnoreCase("none")) {
         return PNReconnectionPolicy.NONE;
@@ -227,15 +334,18 @@ public final class PubNubSubscriber extends StreamingSource<StructuredRecord> {
 
     }
 
+    /**
+     * @return a <code>Boolean</code> type specifying whether to use secure client connection to connect to PubNub.
+     */
+    @Nullable
     public boolean isSecure() {
       return secure;
     }
 
+    /**
+     * @return a <code>String</code> type specifying the authorization key for PubNub.
+     */
     @Nullable
-    public boolean hasProxy() {
-      return useProxy;
-    }
-
     public String getAuthKey() {
       return authKey;
     }
